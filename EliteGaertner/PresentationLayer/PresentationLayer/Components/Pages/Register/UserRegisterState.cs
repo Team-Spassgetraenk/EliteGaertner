71f702fce593735f89059859e34ef16a4800850b
Jonas Kirchenbaur <jonasmax.kirchenbaur@stud.th-rosenheim.de>
2025-12-05 13:16:21 +0100

    PresentationLayer weitergearbeitet

---
+++ EliteGaertner/PresentationLayer/PresentationLayer/Components/Pages/Register/UserRegisterState.cs
@@ -0,0 +1,11 @@
+public class UserRegistrationState
+{
+    // Kontaktdaten
+    public string Email { get; set; } = "";
+    public string Telephone { get; set; } = "";
+    public bool ShareEmail { get; set; }
+    public bool ShareTelephone { get; set; }
+
+    // Ausgewählte Tags
+    public HashSet<string> SelectedTags { get; set; } = new();
+}
